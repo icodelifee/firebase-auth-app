9d034fdae84ae9afa5da6092b8221a6bedb42e1f
icodelifee <mithunsmenon@gmail.com>
2021-05-21 14:32:58 +0530

    feat: add providerscope to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertest/app.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(App());
+  runApp(ProviderScope(child: App()));
 }
