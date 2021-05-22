5968904bd8a1c78e0dac79540619f0f7601610a5
icodelifee <mithunsmenon@gmail.com>
2021-05-23 03:55:27 +0530

    feat: add image and name to drawer

--- lib/ui/homepage/widgets/home_drawer.dart
+++ lib/ui/homepage/widgets/home_drawer.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertest/data/providers/firebase_auth_provider.dart';
+import 'package:fluttertest/ui/homepage/homepage_viewmodel.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 class HomeDrawer extends StatelessWidget {
   const HomeDrawer({
     Key? key,
   }) : super(key: key);
-
+  final _defaultImage = 'https://del.dog/arelebi';
   @override
   Widget build(BuildContext context) {
+    final user = context.read(firebaseAuthProvider).currentUser!;
     return Drawer(
       child: Column(
         children: [
+          UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                  backgroundImage:
+                      NetworkImage(user.photoURL ?? _defaultImage)),
+              decoration: BoxDecoration(color: Colors.red),
+              accountName: Text(
+                user.displayName ?? user.phoneNumber!,
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              accountEmail: Text('UID: ${user.uid}')),
           const Spacer(),
           const Divider(),
           ListTile(
@@ -21,8 +37,20 @@ class HomeDrawer extends StatelessWidget {
                 context.read(firebaseAuthProvider).signOut(),
                 GoogleSignIn().signOut()
               ]);
+              // reset the providers;
+              context.refresh(homePageViewProvider);
+              await context.refresh(restaurantProvider);
             },
-            title: const Text('Logout'),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Logout',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Icon(Icons.logout)
+              ],
+            ),
           )
         ],
       ),
