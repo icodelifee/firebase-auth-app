d65a8d4226822aec533c8dc84799f9a7d601b300
icodelifee <mithunsmenon@gmail.com>
2021-05-22 16:23:47 +0530

    feat: add apiUrl to constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,5 @@ class Constants {
   Constants._();
   static final phoneNumberLimit = 10;
   static final countryCode = '+91';
+  static final apiUrl = 'https://www.mocky.io/v2/5dfccffc310000efc8d2c1ad';
 }
